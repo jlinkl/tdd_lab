6cdd30a7722d7e7760054f79d619c192d698a9b1
James <james.m.linklater@gmail.com>
2022-06-30 15:09:51 +0100

    compound interest done

--- compound_interest/src/compound_interest.py
+++ compound_interest/src/compound_interest.py
@@ -1,6 +1,9 @@
 class CompoundInterest:
-    def do_compound_interest(prinicipal, percent, years):
-        return round(prinicipal * ((1 + percent/12) ** (12*years)), 2)
+    def do_compound_interest(principal, percent, years):
+        return round(principal * ((1 + percent/12) ** (12*years)), 2)
 
     def compound_interest_with_monthly_contribution(principal, percent, years, monthly):
-        return round(principal * ((1 + percent/12) ** (12*years)), 2)
+        p = principal * ((1 + percent/12) ** (12*years))
+        # p = CompoundInterest.do_compound_interest(principal, percent, years)
+        pmt = monthly * (((1 + percent/12) ** (12*years) - 1) / (percent/12)) * (1 + percent/12)
+        return round((p + pmt), 2)
--- compound_interest/tests/compound_interest_test.py
+++ compound_interest/tests/compound_interest_test.py
@@ -24,12 +24,16 @@ class CompoundInterestTest(unittest.TestCase):
 
 
     # Extention tests
+    def test_compound_interest_with_monthly_contribution(self):
+
 
     # Should return 118,380.16 given 100 principal, 5 percent, 8 years, 1000 per month
-
+        self.assertEqual(118380.16, CompoundInterest.compound_interest_with_monthly_contribution(100, 0.05, 8, 1000))
     # Should return 156,093.99 given 100 principal, 5 percent, 10 years, 1000 per month
-
+        self.assertEqual(156093.99, CompoundInterest.compound_interest_with_monthly_contribution(100, 0.05, 10, 1000))
     # Should return 475,442.59 given 116028.86, 7.5 percent, 8 years, 2006 per month
-
+        self.assertEqual(475442.59, CompoundInterest.compound_interest_with_monthly_contribution(116028.86, 0.075, 8, 2006))
+    
     # Should return 718,335.96 given 116028.86 principal, 9 percent, 12 years, 1456 per month
+        self.assertEqual(718335.96, CompoundInterest.compound_interest_with_monthly_contribution(116028.86, 0.09, 12, 1456))
 
